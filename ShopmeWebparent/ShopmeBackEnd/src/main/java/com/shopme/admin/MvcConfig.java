1cf3b5ff2fc1f22646701b092af37e97a2cb8412
usha0320 <162405904+usha0320@users.noreply.github.com>
2024-05-12 21:35:18 +0530

    Add files via upload

--- ShopmeWebparent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebparent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -1,23 +1,41 @@
 package com.shopme.admin;
 
-	 import java.nio.file.Path;
-	import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
-	import org.springframework.context.annotation.Configuration;
-	import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-	import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-	@Configuration
-	public class MvcConfig implements WebMvcConfigurer {
+import com.shopme.admin.paging.PagingAndSortingArgumentResolver;
 
-		@Override
-		public void addResourceHandlers(ResourceHandlerRegistry registry) {
-			
-			String dirName = "user-photos";
-			Path userPhotosDir = Paths.get(dirName);
-			
-		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		exposeDirectory("user-photos", registry);
+		exposeDirectory("../category-images", registry);
+		exposeDirectory("../brand-logos", registry);
+		exposeDirectory("../product-images", registry);
+		exposeDirectory("../site-logo", registry);
+	}
+	
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
 		
-		registry.addResourceHandler("/" + dirName + "/**")
-		        .addResourceLocations("file:/" + userPhotosPath + "/");
-		}}
+		String logicalPath = pathPattern.replace("../", "") + "/**";
+				
+		registry.addResourceHandler(logicalPath)
+			.addResourceLocations("file:/" + absolutePath + "/");		
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new PagingAndSortingArgumentResolver());
+	}
+
+}
